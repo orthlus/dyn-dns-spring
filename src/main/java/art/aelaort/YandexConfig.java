8cc4de54858aa1df6d25200e9428153c8d09bb4f
ort <orthodoxplus@ya.ru>
2024-11-21 02:16:51 +0300

    update ya-iam-helper

--- src/main/java/art/aelaort/YandexConfig.java
+++ src/main/java/art/aelaort/YandexConfig.java
@@ -1,35 +1,15 @@
 package art.aelaort;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-
+@EnableConfigurationProperties(YandexIAMConfig.class)
 @Configuration
 public class YandexConfig {
 	@Bean
-	public YandexIAMSupplier yaIAMSupplier(
-			@Value("${yandex.iAMS3.s3url}") String s3url,
-			@Value("${yandex.iAMS3.s3region}") String s3region,
-			@Value("${yandex.iAMS3.s3id}") String s3id,
-			@Value("${yandex.iAMS3.s3key}") String s3key,
-			@Value("${yandex.iAMS3.s3bucket}") String s3bucket,
-			@Value("${yandex.iAMS3.s3file}") String s3file,
-			@Value("${yandex.iAMS3.funcurl}") URI funcurl,
-			@Value("${yandex.iAMS3.funcsecret}") String funcsecret,
-			RestTemplate yandexDns) {
-		return new YandexIAMSupplier(yandexDns,
-				new YandexIAMConfig(
-						s3id,
-						s3key,
-						s3url,
-						s3region,
-						s3file,
-						s3bucket,
-						funcsecret,
-						funcurl
-				));
+	public YandexIAMSupplier yaIAMSupplier(YandexIAMConfig yandexIAMConfig, RestTemplate yandexDns) {
+		return new YandexIAMSupplier(yandexDns,yandexIAMConfig);
 	}
 }
